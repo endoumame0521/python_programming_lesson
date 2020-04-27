94a96e0866b4bb26151224ab57352ad1c7507c2e
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-27 19:02:46 +0900

    Python_lesson: yaml

--- lesson.py
+++ lesson.py
@@ -1,31 +1,27 @@
 """
-表題: configparserについて
+表題: yaml
 """
 
-import configparser
+import yaml
 
-# config.ini の書き込み
-config = configparser.ConfigParser()
-config['DEFAULT'] = {
-    'debug': True
-}
-config['web_server'] = {
-    'host': '127.0.0.1',
-    'port': 80
-}
-config['db_server'] = {
-    'host': '127.0.0.1',
-    'port': 3306
-}
-with open('config.ini', 'w') as config_file:
-    config.write(config_file)
+# config.ymlの書き込み
+with open('config.yml', 'w') as yaml_file:
+    yaml.dump({
+        'web_server': {
+            'host': '127.0.0.1',
+            'port': 80
+        },
+        'db_server': {
+            'host': '127.0.0.1',
+            'port': 3306
+        }
+    }, yaml_file, default_flow_style=False)
 
-
-# config.ini の読み込み
-config = configparser.ConfigParser()
-config.read('config.ini')
-print(config['web_server'])
-print(config['web_server']['host'])
-print(config['web_server']['port'])
-
-print(config['DEFAULT']['debug'])
+# config.ymlの読み込み
+with open('config.yml', 'r') as yaml_file:
+    data = yaml.safe_load(yaml_file)
+    print(data, type(data))
+    print(data['web_server']['host'])
+    print(data['web_server']['port'])
+    print(data['db_server']['host'])
+    print(data['db_server']['port'])
