2d2018952aebe1fa602c6ddd5728ba26fad914eb
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-28 11:15:31 +0900

    Python_lesson: logging_handler

--- complete/logtest.py
+++ complete/logtest.py
@@ -1,9 +1,13 @@
 import logging
 
+
 # loggingを記述した後はloggerを使ってカスタマイズしていく
 logger = logging.getLogger(__name__)
 # これはloggerを使ってloggingで設定したログレベル(INFO)を(DEBUG)に変更している
 logger.setLevel(logging.DEBUG)
+
+h = logging.FileHandler('logtest.log')
+logger.addHandler(h)
 
 def do_something():
     logger.info('from logtest')
