2cb354d058b6f070281e267349c0472af00daac7
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-29 00:29:52 +0900

    Python_lesson: flask

---
+++ complete/3_WEB and network/flask_lesson/test_flask.py
@@ -0,0 +1,17 @@
+# テスト用にHTTPリクエスト(GET, POST, PUT, DELETE)を作成している
+
+
+import requests
+
+r = requests.get('http://127.0.0.1:5000/employee/jun')
+print(r.text)
+
+r = requests.post('http://127.0.0.1:5000/employee', data={'name': 'jun'})
+print(r.text)
+
+r = requests.put('http://127.0.0.1:5000/employee', data={
+    'name': 'jun', 'new_name': 'sakai'})
+print(r.text)
+
+r = requests.delete('http://127.0.0.1:5000/employee', data={'name': 'jun'})
+print(r.text)
